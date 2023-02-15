eaf984a41b0792e6c28195ee36cc55378555aa67
renat.kovtun23 <renatixlive@gmail.com>
2023-02-15 09:33:57 -0600

    addButton added to the project

--- src/BallPanel.java
+++ src/BallPanel.java
@@ -1,9 +1,11 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
-public class BallPanel extends JPanel {
+public class BallPanel extends JPanel{
     private Color theColor;
     ArrayList<Ball> ballArray = new ArrayList<Ball>();
     int x = 80;
@@ -12,8 +14,20 @@ public class BallPanel extends JPanel {
     int speedY = 3;
 
     Ball oneBall;
+
+
     public BallPanel(Color e)
     {
+
+        JButton addButton = new JButton();
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ballArray.add(new Ball());
+            }
+        });
+        this.add(addButton);
+
         theColor = e;
         setBackground(theColor);
         for(int i = 0; i < 20; i++)
@@ -50,17 +64,22 @@ public class BallPanel extends JPanel {
             public void keyReleased(KeyEvent e) {
 
             }
+
+
         });
     }
 
     @Override
     protected void paintComponent(Graphics g)
     {
+
         super.paintComponent(g);
 
         //super means that you still have methods of original class but you add your method to that list
-        oneBall.drawSpecialBall(g);
         oneBall.teleport(this);
+        oneBall.drawSpecialBall(g);
+
+
         for(Ball b : ballArray)
         {
             b.move(this);
@@ -75,7 +94,6 @@ public class BallPanel extends JPanel {
         repaint();
         //fillOval(x,y,width,height)
     }
-
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class Main{
@@ -6,8 +8,13 @@ public class Main{
     {
         JFrame myFrame = new JFrame();
         myFrame.setSize(500,500);
+
         //DrawPanel myPanel = new DrawPanel(Color.orange);
         BallPanel bPanel = new BallPanel(Color.ORANGE);
+        bPanel.setFocusable(true);
+
+
+
         myFrame.add(bPanel);
         myFrame.setVisible(true);
 
